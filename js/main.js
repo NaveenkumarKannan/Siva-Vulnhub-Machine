5101c7ae037c4df43f4842b2ad5c3ed2fefc1291
NaveenkumarKannan <46358531+NaveenkumarKannan@users.noreply.github.com>
2020-07-23 19:02:16 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+//console.log($(input).val());
+(function ($) {
+    "use strict";
+
+
+    $(".forgot").click(function() {
+        var email = $('.email');
+        var thisAlert = $(email).parent();
+        if($(email).val().trim() == ''){
+            $(thisAlert).addClass('alert-validate');
+        }else if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) != null) {
+            alert("Instructions on how to reset your password were sent to "+$(email).val());    
+        }else{
+            $(thisAlert).addClass('alert-validate');
+        }
+    });
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
